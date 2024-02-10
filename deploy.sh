0ff4962d01c875af931b9e3dbd04577eea7c8488
Nico Wunder <n.wunder@onacy.de>
2024-02-10 18:34:56 +0100

    handle git

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,18 @@ echo "ewww"
 apt install git -y
 echo "hi"
 cd /dir
-git clone https://github.com/Wunder-TV/shopkware.git
+# git clone https://github.com/Wunder-TV/shopkware.git
+# Define the directory where your repo should be or will be cloned
+# Define the Git repository URL
+GIT_REPO_URL="https://github.com/Wunder-TV/shopkware.git"
+
+# Check if the directory exists
+if [ -d ".git" ]; then
+  echo "Repository directory exists. Attempting to update..."
+  # Pull the latest changes
+  git pull
+else
+  echo "Repository directory does not exist. Cloning the repository..."
+  # Clone the repository
+  git clone "$GIT_REPO_URL" "."
+fi
